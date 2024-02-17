b432eea9a10e92fc3974bcc04fbb42b093424230
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2024-02-17 02:17:33 +0100

    feat: :zap: Client-side code implementation
    The user is asked for the string to search for, the Merkle Proof is taken and sent to the server where further checks will be carried out

--- client/index.js
+++ client/index.js
@@ -1,14 +1,31 @@
 const axios = require('axios');
-const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  // create the merkle tree for the whole nice list
+  // --------------------------------------------
+  // Completed implementation of the feature
+  const index= niceList.findIndex(n => n ===process.argv[2])
+  if(!process.argv[2]){
+    console.log("Please enter a valid name from the list and a name to send to the server, run `node client/index '<string>'`")
+    process.exit()
+  }
+
+  const leaf=process.argv[2]
+  const merkleTree= new MerkleTree(niceList)
+  const proof =  merkleTree.getProof(index)
+  console.log(`Proof # of nodes: ${proof.length}`)
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    // TODO: add request body parameters here! 
+    // --------------------------------------------
+    // Completed implementation of the feature
+    proof,
+    leaf
   });
 
   console.log({ gift });
